41ba6a7625ebaf958a921cce097d5ce706780574
Gabriel Correa <62802360+GabrielCDias@users.noreply.github.com>
2023-02-18 14:13:15 -0300

    Ajustando Classes -Envento/Endereço/Usuario

--- src/evento/Evento.java
+++ src/evento/Evento.java
@@ -1,15 +1,75 @@
-package evento;
-
-
 import java.util.Date;
 
 public class Evento {
+    private String nome;
+    private Date dataInicio;
+    private Date dataFim;
+    private Endereco endereco;
+    private String organizadores;
+    private String detalhes;
+    //Falta o folder
 
-    String nome;
-    Date Inicio;
-    Date fim;
-   String local; // pode criar uma  classe endereço
-    String detalhes;
 
-    // folder;
+    //Contrutor
+
+    public Evento(String nome, Date dataInicio, Date dataFim, Endereco endereco, String organizadores, String detalhes) {
+        this.nome = nome;
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+        this.endereco = endereco;
+        this.organizadores = organizadores;
+        this.detalhes = detalhes;
+ 
+    }
+
+    //Get e Set
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(Date dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public Date getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(Date dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getOrganizadores() {
+        return organizadores;
+    }
+
+    public void setOrganizadores(String organizadores) {
+        this.organizadores = organizadores;
+    }
+
+    public String getDetalhes() {
+        return detalhes;
+    }
+
+    public void setDetalhes(String detalhes) {
+        this.detalhes = detalhes;
+    }
+
 }
---
+++ src/usuario/Usuario.java
@@ -0,0 +1,57 @@
+public class Usuario {
+    private String matricula;
+    private String senha;
+    private String nome;
+    private String email;
+    private String telefone;
+
+    // Contrutor
+    public Usuario(String matricula, String senha, String nome, String email, String telefone) {
+        this.matricula = matricula;
+        this.senha = senha;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    // getters e setters
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
