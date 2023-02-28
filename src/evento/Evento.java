9852bd10de1b5b3cde2398e4df18feb23076fc0b
Gabriel Correa <62802360+GabrielCDias@users.noreply.github.com>
2023-02-28 17:31:50 -0300

    Classes
    Ajuste de classes.

--- src/evento/Evento.java
+++ src/evento/Evento.java
@@ -1,15 +1,96 @@
 package evento;
 
+import endereco.Endereco;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import java.time.format.DateTimeFormatter;
+
 public class Evento {
+    private String nome;
+    private Endereco endereco;
+    private String organizadores;
+    private String detalhes;
+    //Falta o folder
+    private Date dataInicio;
+    private Date dataFim;
+    private Date horario;
 
-    String nome;
-    Date Inicio;
-    Date fim;
-   String local; // pode criar uma  classe endereço
-    String detalhes;
 
-    // folder;
+    private SimpleDateFormat horarioFormatado = new SimpleDateFormat("HH:mm");
+    private SimpleDateFormat dataFormatada = new SimpleDateFormat("dd/MM/yyyy");
+
+
+    public Evento(String nome, String dataInicio , String horario, String dataFim, Endereco endereco, String organizadores, String detalhes) {
+        this.nome = nome;
+        try {
+            this.dataInicio = dataFormatada.parse(java.lang.String.valueOf(dataInicio));
+            this.dataFim = dataFormatada.parse(java.lang.String.valueOf(dataFim));
+            this.horario = horarioFormatado.parse(java.lang.String.valueOf(horario));
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        this.endereco = endereco;
+        this.organizadores = organizadores;
+        this.detalhes = detalhes;
+        //this.folder = folder;
+    };
+/*
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(Date dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public Date getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(Date dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getOrganizadores() {
+        return organizadores;
+    }
+
+    public void setOrganizadores(String organizadores) {
+        this.organizadores = organizadores;
+    }
+
+    public String getDetalhes() {
+        return detalhes;
+    }
+
+    public void setDetalhes(String detalhes) {
+        this.detalhes = detalhes;
+    } */
+
+    @Override
+    public String toString(){
+            return "Evento "+this.nome+ " na data "+dataFormatada.format(this.dataInicio)+ " - começa "+ horarioFormatado.format(this.horario) + " no Endereço: "+this.endereco;
+    };
+
 }
